853bb26c21acef10836776eea6c92e521e557b9b
mohamed khaled <123021751+Kavloa@users.noreply.github.com>
2024-03-26 15:01:25 +0200

    Delete alx-files_manager-main directory

--- alx-files_manager-main/utils/extractToken.js
+++
@@ -1,6 +0,0 @@
-const extractToken = (req) => {
-  const token = req.headers['x-token'];
-  return token;
-};
-
-export default extractToken;
--- alx-files_manager-main/utils/hashPassword.js
+++
@@ -1,5 +0,0 @@
-const sha1 = require('sha1');
-
-const hashPassword = (password) => sha1(password);
-
-export default hashPassword;
